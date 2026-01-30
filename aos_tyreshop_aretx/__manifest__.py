beee3147245aa6176e9c743742db05e9da30dfa5
Shrey <shreybaviskar22@gmail.com>
2026-01-30 17:23:36 +0530

    Added Tyreshop module working with both vehicle and workshop & all functions working perfectly

---
+++ aos_tyreshop_aretx/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Tyreshop",
+    'version': '19.0.1.0.0',
+    'summary': """For Warrarnty """,
+    'description': """This Module Enables Warranty field""",
+    'author': 'areterix',
+    'company': 'Areterix Technologies',
+    'website': "https://www.areterix.com",
+    'category': 'Accounting',
+    'depends': ['base', 'account', 'stock', 'payment'],
+    'data': [
+        'views/warranty.xml'
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
