3683ed258a2930d9f9263ae3a2993dd0e594e8a0
Shrey <shreybaviskar22@gmail.com>
2026-02-10 11:35:43 +0530

    changed reminder policy settings and added contacts searchview

--- aos_vehicle_aretx/models/customer_line.py
+++ aos_vehicle_aretx/models/customer_line.py
@@ -9,4 +9,19 @@ class CustomContacts(models.Model):
     x_vehicle_number_ids = fields.Many2many('vehicle.master.model',string="Brand Name", ondelete='restrict')
     x_color_ids = fields.One2many('vehicle.master.model', 'x_color_id', string="Vehicle Details",ondelete='restrict')
 
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
 
+    vehicle_numbers = fields.Char(
+        string="Vehicle Numbers",
+        compute="_compute_vehicle_numbers",
+        store=True,
+        index=True,
+    )
+
+    @api.depends('x_vehicle_number_ids', 'x_vehicle_number_ids.x_vehicle_number_id')
+    def _compute_vehicle_numbers(self):
+        for partner in self:
+            partner.vehicle_numbers = ', '.join(
+                partner.x_vehicle_number_ids.mapped('x_vehicle_number_id')
+            )
