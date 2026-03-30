43a10c4c56b5c8d77bbc90cc877d0e741c8080e8
Shrey <shreybaviskar22@gmail.com>
2026-03-30 16:58:55 +0530

    Added new client module to migrate to odoo19

---
+++ aretx_autocraft_main/README.md
@@ -0,0 +1,1 @@
+# aretx_autocraft
---
+++ aretx_autocraft_main/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Aretx Autocraft',
+    'version': '19.0.1.0.0',
+    'summary': 'Vehicle Details for Workshop',
+    'category': 'Industry',
+    'author': 'Aretrix',
+    'depends': ['base', 'crm', 'sale'],
+    'data': [
+        'views/autocraft.xml',
+        # 'views/project_project_view1.xml',
+        'views/list_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
