9ae0da4ea490de45f6a96fc483973de2c40d0e8a
Shrey <shreybaviskar22@gmail.com>
2026-03-30 12:53:39 +0530

    Fixed the issue causing whatsapp message error and blank screen error

--- aos_employee_expense_aretx/__init__.py
+++ aos_employee_expense_aretx/__init__.py
@@ -1,1 +1,8 @@
 from . import models
+
+def uninstall_hook(env):
+    # Restore the original contacts action when uninstalling
+    original_action = env.ref('contacts.action_contacts', raise_if_not_found=False)
+    menu = env.ref('contacts.menu_contacts', raise_if_not_found=False)
+    if menu and original_action:
+        menu.action = original_action
